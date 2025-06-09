21497fca8b586062f5e5dd4b74157f1792212ca0
NV-Nandini <85825135+NV-Nandini@users.noreply.github.com>
2025-06-09 12:47:48 -0500

    Time: 4 ms (78.12%), Space: 42.9 MB (88.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char S[]=s.toCharArray();
-        char T[]=t.toCharArray();
-        Arrays.sort(S);
-        Arrays.sort(T);
-        return Arrays.equals(S, T);
+        if(s.length()!=t.length() ){//if the lengths do no match, they can't be anagrams
+            return false;
+        }
+        int freq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+           freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int count:freq){
+            if(count!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
